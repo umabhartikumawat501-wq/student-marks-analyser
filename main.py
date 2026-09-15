5df570bd2de5a67b66d8b8a6745c30537ef71044
umabhartikumawat501-wq <umabhartikumawat501@gmail.com>
2026-09-15 23:12:17 +0530

    cgpa calculation

--- main.py
+++ main.py
@@ -1,99 +1,14 @@
+# Calculate CGPA
 
+grade_points = {
+    "A+": 10,
+    "A": 9,
+    "B": 8,
+    "B+": 7,
+    "D": 6,
+    "F": 0
+}
 
-import csv
-import os
+cgpa = grade_points[grade]
 
-
-
-name = input("Enter student full name: ")
-
-
-subjects = ["Python", "Database", "Maths", "English", "Computer" ]
-
-marks = []
-
-
-for subject in subjects:
-
-    mark = float (input(f"Enter marks for {subjects}: "))
-
-    marks.append(mark)
-
-total = sum(marks)
-
-average = total / len(marks)
-
-if average >= 90:
-
-    grade = "A+"
-
-elif average >= 80:
-
-    grade = "A"
-
-elif average >= 70:
-
-    grade = "B"
-
-elif average >= 60:
-
-    grade = "B+"
-
-elif average >= 50:
-
-    grade = "D"
-
-else:
-
-    grade = "F"
-
-result = "PASS" if average >= 40 else "FAIL"
-
-print(f"Student Name :  {name}")
-
-print(f"Total Marks  :  {total}")
-
-print(f"Average      :  {average:.2f}")
-
-print(f"Grade        :  {grade}")
-
-print(f"Result       :  {result}")
-file_name = "student_results.csv"
-
-file_exists = os.path.exists(file_name)
-
-with open(file_name, "a", newline="") as file:
-
-    writer = csv.writer(file)
-
-    if not file_exists:
-
-        writer.writerow([
-
-            "Student Name",
-
-            "Total",
-
-            "Average",
-
-            "Grade",
-
-            "Result",
-
-        ])
-
-        writer.writerow([
-
-            name,
-
-            total,
-
-            round(average, 2),
-
-            grade,
-
-            result
-
-        ])
-
-print("\nResults saved to student_results.csv")
+print(f"CGPA         :  {cgpa:.2f}")
